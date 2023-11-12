2f5d254f908e07f4a7c0858862d6b18afda947e1
Daniel <daniel.liemng@gmail.com>
2023-11-12 11:20:56 -0500

    category entity

---
+++ app/(dashboard)/[storeId]/(routes)/categories/_components/cell-action.tsx
@@ -0,0 +1,97 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { CategoryColumn } from './column';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Copy, Edit, MoreHorizontal, Trash } from 'lucide-react';
+import toast from 'react-hot-toast';
+import { useParams, useRouter } from 'next/navigation';
+import axios from 'axios';
+import { useState } from 'react';
+import AlertModal from '@/components/modals/alert-modal';
+
+interface CellActionProps {
+  data: CategoryColumn;
+}
+
+const CellAction = ({ data }: CellActionProps) => {
+  const router = useRouter();
+  const params = useParams();
+
+  const [open, setOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onCopy = (id: string) => {
+    navigator.clipboard.writeText(id);
+    toast.success('ID copied');
+  };
+
+  const onDelete = async () => {
+    try {
+      setIsLoading(true);
+
+      await axios.delete(`/api/${params.storeId}/categories/${data.id}`);
+
+      router.refresh();
+
+      toast.success('Category deleted');
+    } catch (err) {
+      console.log(err);
+      toast.error(
+        'Make sure you removed all products using this category first'
+      );
+    } finally {
+      setIsLoading(false);
+      setOpen(false);
+    }
+  };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        isLoading={isLoading}
+      />
+
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant='ghost' className='h-8 w-8 p-0'>
+            <span className='sr-only'>Open menu</span>
+            <MoreHorizontal className='h-4 w-4' />
+          </Button>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent align='end'>
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+
+          <DropdownMenuItem onClick={() => onCopy(data.id)}>
+            <Copy className='mr-2 w-4 h-4' />
+            Copy Id
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() =>
+              router.push(`/${params.storeId}/categories/${data.id}`)
+            }
+          >
+            <Edit className='mr-2 w-4 h-4' />
+            Update
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setOpen(true)}>
+            <Trash className='mr-2 w-4 h-4' />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
+
+export default CellAction;
---
+++ app/(dashboard)/[storeId]/(routes)/categories/_components/column.tsx
@@ -0,0 +1,85 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { Checkbox } from '@/components/ui/checkbox';
+import { ColumnDef } from '@tanstack/react-table';
+import { ArrowUpDown, MoreHorizontal } from 'lucide-react';
+import CellAction from './cell-action';
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type CategoryColumn = {
+  id: string;
+  name: string;
+  billboardLabel: string;
+  createdAt: string;
+};
+
+export const columns: ColumnDef<CategoryColumn>[] = [
+  {
+    id: 'select',
+    header: ({ table }) => (
+      <Checkbox
+        checked={table.getIsAllPageRowsSelected()}
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label='Select all'
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label='Select row'
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: 'name',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Name
+          <ArrowUpDown className='ml-2 h-4 w-4' />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: 'billboard',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Billboard Label
+          <ArrowUpDown className='ml-2 h-4 w-4' />
+        </Button>
+      );
+    },
+    cell: ({ row }) => row.original.billboardLabel,
+  },
+  {
+    accessorKey: 'createdAt',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Date
+          <ArrowUpDown className='ml-2 h-4 w-4' />
+        </Button>
+      );
+    },
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => <CellAction data={row.original} />,
+  },
+];
